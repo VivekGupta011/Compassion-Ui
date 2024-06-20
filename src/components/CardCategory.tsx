d50b9b0aad8cf0b00ee7589c30c3e1629890b6d1
vivek gupta <viveksite01@gmail.com>
2024-06-20 13:07:30 +0530

    feat:card update

--- src/components/CardCategory.tsx
+++ src/components/CardCategory.tsx
@@ -1,5 +1,6 @@
 "use client";
 import SaveChildIcon from "@/constants/AssetsSvg/SaveChildIcon";
+import StarIcon from "@/constants/AssetsSvg/StarIcon";
 import React from "react";
 
 const CardCategory = ({ CardData }: any) => {
@@ -15,8 +16,22 @@ const CardCategory = ({ CardData }: any) => {
           </button>
         </div>
         <div>
-          <button className="bg-gray-300 text-gray-800 px-4 py-2 rounded">
+          <button className="bg-gray-300 text-gray-800 px-4 py-2 rounded flex items-center">
             Sort by
+            <svg
+              className="w-8 h-7 ml-2 text-custom-blue"
+              fill="currentColor"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
           </button>
         </div>
       </div>
@@ -27,27 +42,39 @@ const CardCategory = ({ CardData }: any) => {
           const giftType = item.fields.giftType.replace("New", "").trim();
 
           return isNewGiftType ? (
-            <div key={item.sys.id} className="bg-bg-sky-blue shadow-md rounded-lg overflow-hidden relative">
+            <div
+              key={item.sys.id}
+              className="bg-bg-sky-blue shadow-md rounded-lg overflow-hidden relative"
+            >
               <div className="absolute top-0 bg-bg-sky-blue left-0 text-custom-blue font-bold px-6 py-3 text-lg rounded-br flex items-center space-x-2">
                 <SaveChildIcon />
                 <span>{giftType}</span>
               </div>
 
-              <img
-                className="w-full object-cover rounded-lg p-2 lg:p-4 min-h-[280px] md:min-h-[350px]"
-                src={`https:${item.fields.giftBgImage.fields.file.url}`}
-                alt="School Textbooks"
-              />
+              <div className="relative z-0">
+                <img
+                  className="w-full object-cover rounded-lg p-2 lg:p-4 min-h-[280px] md:min-h-[350px]"
+                  src={`https:${item.fields.giftBgImage.fields.file.url}`}
+                  alt="School Textbooks"
+                />
+                <div className="absolute bottom-0 left-0 bg-yellow-300 text-black text-sm font-bold px-4 uppercase py-3 rounded-tr-lg m-2 overflow-hidden lg:m-4">
+                  New
+                </div>
+              </div>
 
               <div className="p-4">
                 <h3 className="font-bold text-Card-text-color text-2xl ">
-                {item.fields.giftType}
+                  {item.fields.giftType}
                 </h3>
               </div>
 
               <div className="flex justify-between items-center mt-4 mx-3 border-t-2 border-add-top-border z-50 py-3 px-3">
-                <p className="mt-2 text-Card-text-color font-bold text-2xl"> £{item.fields.giftAmount}</p>
-                <button className="text-add-btn-bg font-semibold">Learn More</button>
+                <p className="mt-2 text-Card-text-color font-bold text-2xl">
+                  £{item.fields.giftAmount}
+                </p>
+                <button className="text-add-btn-bg font-semibold">
+                  Learn More
+                </button>
                 <button className="bg-add-btn-bg max-w-[200px] text-white px-5 py-2 rounded font-semibold">
                   Add
                 </button>
@@ -70,7 +97,8 @@ const CardCategory = ({ CardData }: any) => {
                   <p className="mt-2 text-white text-base font-bold">
                     From{" "}
                     <span className="block text-2xl">
-                      £{item.fields.giftAmount} <span className="text-sm">/PM</span>
+                      £{item.fields.giftAmount}{" "}
+                      <span className="text-sm">/PM</span>
                     </span>
                   </p>
 
@@ -86,15 +114,31 @@ const CardCategory = ({ CardData }: any) => {
             </div>
           );
         })}
+
+        {/* last E-cards */}
+        <div className="bg-custom-blue relative flex min-h-[386px] p-4 shadow-lg md:min-h-[500px] rounded-lg">
+          <div className="flex justify-center items-center py-12 px-4 flex-col">
+            <div className="pb-1 lg:pb-4">
+              <StarIcon />
+            </div>
+            <h2 className="text-2xl text-white py-3 lg:py-4 font-bold">
+              Personalised e-cards
+            </h2>
+            <p className="text-center text-white text-lg">
+              Your loved one will receive a virtual e-card, which you can
+              personalise, telling them about the impact their gift will make.
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="flex justify-between items-center mt-8">
-        <button className="text-blue-600">&lt; Previous</button>
-        <div className="text-black px-4 py-2 rounded-lg font-semibold">
+        <button className="text-gray-400">&lt; Previous</button>
+        <div className="text-gray-700 px-4 py-2 rounded-lg">
           Page{" "}
           <span className="border border-blue-600 py-3 px-4 rounded-md">1</span>
         </div>
-        <button className="text-blue-600">Next &gt;</button>
+        <button className="text-gray-400">Next &gt;</button>
       </div>
     </div>
   );
---
+++ src/constants/AssetsSvg/StarIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const StarIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="76"
+      height="102"
+      viewBox="0 0 76 102"
+    >
+      <path
+        fill="#fed100"
+        d="M34.375 20.31h47.993V1.043z"
+        data-name="Path 1407"
+        transform="translate(-28.861 -1.043)"
+      ></path>
+      <path
+        fill="#fed100"
+        d="M13.75 23.543v78.562h76V23.543zm52.414 61.2L51.75 74.325 37.336 84.742l5.459-16.712L28.6 57.613h17.69L51.75 40.9l5.458 16.712H74.9L60.484 68.03z"
+        data-name="Path 1408"
+        transform="translate(-13.75 -.105)"
+      ></path>
+    </svg>
+  );
+};
+
+export default React.memo(StarIcon);
